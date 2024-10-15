7b3457c4e20bc604da4c112a3fa0bf38ce2a27d0
Danielggz <dggz1993@gmail.com>
2024-10-15 21:03:46 +0100

    Add custom exception class

---
+++ src/pkg22212337_daniel_client/IncorrectActionException.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package pkg22212337_daniel_client;
+
+/**
+ * @author Daniel García
+ * 15/10/2024
+ */
+public class IncorrectActionException extends Exception{
+    private String message = "Action selected is not valid. Please select Add, List or Stop";
+
+    public IncorrectActionException() {
+    }
+
+    public IncorrectActionException(String message) {
+        super(message);
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
